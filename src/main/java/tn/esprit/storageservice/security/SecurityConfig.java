dd00ca7c20ba99b3fe1e2eedf0fe9ab717fd6d1b
Maram-web <maram.manai@esprit.tn>
2025-07-06 21:21:42 +0200

    actuar/health done

--- src/main/java/tn/esprit/storageservice/security/SecurityConfig.java
+++ src/main/java/tn/esprit/storageservice/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/storage/actuator/health").permitAll() // ✅ AJOUT ICI
                         .requestMatchers("/api/storage/s3/**").hasRole("USER")
                         .requestMatchers("/files/**").permitAll()
 
